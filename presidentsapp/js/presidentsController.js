045e4f1d9aae96471fe45c5e0acbbd69d6704d64
Denis Cheung <d.s.h.cheung@gmail.com>
2015-12-01 14:09:25 +0800

    solution

--- presidentsapp/js/presidentsController.js
+++ presidentsapp/js/presidentsController.js
@@ -1,5 +1,5 @@
 angular.module('ThePresidentsApp', [])
-  .controller('PresidentsController', ['$scope', function PresidentsController($scope){
+  .controller('PresidentsController', ['$scope', '$http', function PresidentsController($scope, $http){
   $scope.all = [
     {name: 'George Washington', start: 1789, end: 1797 },
     {name: 'John Adams', start: 1797, end: 1801 },
@@ -9,9 +9,33 @@ angular.module('ThePresidentsApp', [])
 
   $scope.newPresident = {};
   $scope.addPresident = addPresident;
+  $scope.getPresidents = getPresidents;
+  $scope.deletePresident = deletePresident;
+
+  getPresidents();
+  function getPresidents(){
+    $http
+      .get('http://localhost:3000/presidents')
+      .then(function(response){
+        $scope.all = response.data.presidents;
+    });
+  }
 
   function addPresident(){
-    $scope.all.push($scope.newPresident);
+    $http
+      .post('http://localhost:3000/presidents', $scope.newPresident)
+      .then(function(response){
+        getPresidents();
+    });
     $scope.newPresident = {};
+  }
+
+  function deletePresident(president){
+    $http
+      .delete("http://localhost:3000/presidents/" + president._id)
+      .then(function(response){
+        var index = $scope.all.indexOf(president);
+        $scope.all.splice(index, 1);
+      });
   }
 }]);
